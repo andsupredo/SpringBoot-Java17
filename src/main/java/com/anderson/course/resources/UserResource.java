b4298664b77cd57cbf0949c4b85961dd0f3b9f07
andersoo.sousa@gmail.com <andersoo.sousa@gmail.com>
2022-08-30 12:10:38 -0300

    User entity and resource

---
+++ src/main/java/com/anderson/course/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.anderson.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.anderson.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User (1L, "Anderson", "anderson@gmail.com", "41999999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
+
