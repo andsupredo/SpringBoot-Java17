78ce7d12457958604d7d54087d57db38615a3feb
andersoo.sousa@gmail.com <andersoo.sousa@gmail.com>
2022-08-31 08:41:41 -0300

    H2 database, test profile, JPA

--- src/main/java/com/anderson/course/entities/Users.java
+++ src/main/java/com/anderson/course/entities/Users.java
@@ -1,22 +1,28 @@
 package com.anderson.course.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 
-public class User implements Serializable {
-	
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Users implements Serializable {
 	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
-	
-	public User() {
-		
+
+	public Users() {
 	}
 
-	public User(Long id, String name, String email, String phone, String password) {
+	public Users(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -67,7 +73,10 @@ public class User implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -78,9 +87,12 @@ public class User implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		User other = (User) obj;
-		return Objects.equals(id, other.id);
+		Users other = (Users) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-
-	
 }
--- src/main/java/com/anderson/course/resources/UserResource.java
+++ src/main/java/com/anderson/course/resources/UserResource.java
@@ -1,21 +1,20 @@
 package com.anderson.course.resources;
 
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.anderson.course.entities.User;
+import com.anderson.course.entities.Users;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User (1L, "Anderson", "anderson@gmail.com", "41999999999", "12345");
+	public ResponseEntity<Users> findAll() {
+		Users u = new Users(1L, "Maria", "maria@gmail.com", "9999999", "12345");
 		return ResponseEntity.ok().body(u);
 	}
-
 }
-
