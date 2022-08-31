6dadd4e1e8d55b3b87600d3923bfeb327e815aa1
andersoo.sousa@gmail.com <andersoo.sousa@gmail.com>
2022-08-31 09:28:59 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/anderson/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.anderson.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.anderson.course.entities.Users;
+
+public interface UserRepository extends JpaRepository<Users, Long>{
+
+}
