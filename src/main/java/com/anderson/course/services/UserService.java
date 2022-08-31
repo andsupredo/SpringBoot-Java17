fde50665a671a486c9206477aab05ed04bea04b6
andersoo.sousa@gmail.com <andersoo.sousa@gmail.com>
2022-08-31 10:01:23 -0300

    Service layer, component registration

---
+++ src/main/java/com/anderson/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.anderson.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.anderson.course.entities.Users;
+import com.anderson.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<Users>findAll() {
+		return repository.findAll();
+	}
+	public Users findById(Long id) {
+		Optional<Users> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+
+}
