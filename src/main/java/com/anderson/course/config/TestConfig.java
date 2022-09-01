ee556ead6d32677c13534bbd6d980bf14f1560ae
andersoo.sousa@gmail.com <andersoo.sousa@gmail.com>
2022-09-01 12:27:13 -0300

    Payment, one-to-one association

--- src/main/java/com/anderson/course/config/TestConfig.java
+++ src/main/java/com/anderson/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.anderson.course.entities.Category;
 import com.anderson.course.entities.Order;
 import com.anderson.course.entities.OrderItem;
+import com.anderson.course.entities.Payment;
 import com.anderson.course.entities.Product;
 import com.anderson.course.entities.Users;
 import com.anderson.course.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-21T19:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.saveAll(Arrays.asList(o1));	
 	}
 
 }
